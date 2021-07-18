7744811e31ed6c8c17d5a4787411a63ef3070e91
Rishav Sarkar <76059331+rishavsarkar221@users.noreply.github.com>
2021-07-18 15:28:26 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -221,17 +221,9 @@ let banned_words = [
 
 client.on('guildMemberAdd', async newMember => {
     const welcomeChannel = newMember.guild.channels.cache.find(channel => channel.name === '🤖new-users ')
-    welcomeChannel.send('')
-
-
-    // Optional Part (you can modify those extra things if you'd like!) :D
-    let msgEmbed = new Discord.MessageEmbed()
-    .setTitle (`This is a title for a test`)
-    // welcomeChannel.send(msgEmbed) | (that's commented so you know to use it only if you want an embed and also don't delete the other "welcomeChannel.send" just change it in there and say "welcomeChannel.send(msgEmbed)" and define the msgEmbed variable as a let and define it above the "welcomeChannel.send" so the bot will check and see that it's defined so errors won't happen!
+    welcomeChannel.send('Heya Man! Good to see you.\nPlease Read our Rules before chatting in our server 😁😁')
+    
     if (newMember.bot) return; // checks if it's a bot that joined so the channel won't be spammed with "*Discord Bot* has joined the server" and stuff like that, so check that.
-    const newbieRole = newMember.roles.cache.find(role => role.name === 'Role Name here') // that was to define the role to give newbies (you can name the variable however you want that doesn't matter!)
-    newMember.roles.add(newbieRole.id) // this will add the role to that member!
-    // All the things that are under the "Optional Part" are 100 % Optional! No Requirement to use those, just use it if you want and they won't affect the welcome message at all!
 })
 
 client.on('message', (message) => {
