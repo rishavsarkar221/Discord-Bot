45867e3c39b5097f42ac7a56484240756afd4333
Rishav Sarkar <76059331+rishavsarkar221@users.noreply.github.com>
2021-07-18 15:19:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -217,6 +217,23 @@ let banned_words = [
     "```chatann```"
 ]
 
+// IMPORTANT NOTE: Make Sure To Use async and rename bot to client or whatever name you have for your bot events!
+
+client.on('guildMemberAdd', async newMember => {
+    const welcomeChannel = newMember.guild.channels.cache.find(channel => channel.name === '🤖new-users ')
+    welcomeChannel.send('')
+
+
+    // Optional Part (you can modify those extra things if you'd like!) :D
+    let msgEmbed = new Discord.MessageEmbed()
+    .setTitle (`This is a title for a test`)
+    // welcomeChannel.send(msgEmbed) | (that's commented so you know to use it only if you want an embed and also don't delete the other "welcomeChannel.send" just change it in there and say "welcomeChannel.send(msgEmbed)" and define the msgEmbed variable as a let and define it above the "welcomeChannel.send" so the bot will check and see that it's defined so errors won't happen!
+    if (newMember.bot) return; // checks if it's a bot that joined so the channel won't be spammed with "*Discord Bot* has joined the server" and stuff like that, so check that.
+    const newbieRole = newMember.roles.cache.find(role => role.name === 'Role Name here') // that was to define the role to give newbies (you can name the variable however you want that doesn't matter!)
+    newMember.roles.add(newbieRole.id) // this will add the role to that member!
+    // All the things that are under the "Optional Part" are 100 % Optional! No Requirement to use those, just use it if you want and they won't affect the welcome message at all!
+})
+
 client.on('message', (message) => {
     const promoteMsg = `You know, you can follow the developer of this bot over here:
     Youtube: https://www.youtube.com/channel/UCoRAQSN0v0PxOPBGhajIlbQ
@@ -233,8 +250,8 @@ client.on('message', (message) => {
     
     const randomNumber = Math.floor(Math.random() * wannaPromote.length)
     
-    words = message.content.toLowerCase().split(" ");
-    banned_words_used = []
+    let words = message.content.toLowerCase().split(" ");
+    let banned_words_used = []
 
     for (i = 0; i < words.length; i++) {
         check_word = banned_words.indexOf(words[i])
@@ -293,6 +310,30 @@ client.on('message', (message) => {
         banned_words.forEach(banned => banned)
         if (wannaPromote[randomNumber] == 'yes') {
         message.channel.send(promoteMsg)
+        }
+    }
+    
+    let check_name = message.content.toLowerCase().split(" ");
+    let banned_name_used = []
+
+    for (i = 0; i < words.length; i++) {
+        check_name = banned_words.indexOf(words[i])
+        banned_name_used.push(banned_words[check_word])
+
+        if(check_name !== -1) {
+            if(message.author == "862214788393861141") {
+                return;
+            }
+            else {
+                message.delete()
+                message.reply("Hey You have a banned name!!! Change it 😡😡")
+                message.guild.members.get(message.author).setNickname("Broke rules, so changed nickname")
+
+                if (wannaPromote[randomNumber] == 'yes') {
+                    message.channel.send()
+                }
+                break;
+            }
         }
     }
 })
